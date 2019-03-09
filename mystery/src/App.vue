151f2944020d6a8b0ef832d27cba03d73812de75
kazufumi-nishida <kazufumi_nishida@dwango.co.jp>
2019-03-09 14:53:05 +0900

    タイトル・ドロップダウンメニュー、概要の表示などを追加

--- mystery/src/App.vue
+++ mystery/src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>ミステリーラリー(仮)</h1>
+    <select name="rally" v-model="selectedRally">
+      <option v-for="rally in rallies" :key='rally.id' :id="rally.id" v-bind:value="rally">
+        {{ rally.title }}
+      </option>
+    </select>
+
+    <h3>{{ selectedRally.title }}</h3>
+    <div>{{ selectedRally.desc }}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'app',
+  data: () => {
+    return {
+      rallies: [
+        { id: 1, title: "神社巡り", desc: "神社を巡って運を高めよう！" },
+        { id: 2, title: "高級和牛を目指せ!", desc: "ゴールに到達して割引クーポンを得よう！" }
+      ],
+      selectedRally: ""
+    };
+  },
   components: {
-    HelloWorld
+  },
+  mounted() {
   }
 }
 </script>
